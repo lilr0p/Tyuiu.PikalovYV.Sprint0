4a8b1938669b2f9b0196d83ac38d228f453da4c0
pikal <pikal@LAPTOP-5JDVG4R6>
2025-08-27 14:00:31 +0500

    Добавлен Task1 (теперь точно)

---
+++ Tyuiu.PikalovYV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PikalovYV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            String name = "Юра";
+            int age = 20;
+            Console.WriteLine("Зравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
+// Юра был здесь
+
